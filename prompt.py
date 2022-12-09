0ad46049a3ce3b2d28aa76fddd5d1ea1e7f736ef
Chirag <76880977+chirag127@users.noreply.github.com>
2022-12-09 19:11:08 +0530

    prompt

--- prompt.py
+++ prompt.py
@@ -3,7 +3,7 @@ def return_prompt(lyrics, artist,song_name):
 
 I know everything about you. Please do the following task for me and Please don't disappoint me.
 
-I am try to make a blog which contains the analysis, review, explanation of the song {song_name} by {artist}.
+I am try to make a blog which contains the analysis, review, explanation etc of the song {song_name} by {artist}.
 
 Lyrics for {song_name} by {artist} are as follows:
 
@@ -16,9 +16,9 @@ Now, please try to do the following task for me:
     2. Try to give the verse-by-verse, complete, in-depth explaination and meaning for the song {song_name} by {artist} whose lyrics are given above. Other details for this are:
 
         A. Try to explain the meaning of the first verse, second verse, third verse, etc. of the song {song_name} by {artist} whose lyrics are given above. for example, you can say that the first verse of the song is about the following things: ((first verse explaination)).
-        B. You may try to also explain the meaning of the chorus, bridge, etc. of the song. for example, you can say that the chorus of the song is about the following things: ((chorus explaination)).
-        C. You may try to explain the chorus only once. You don't need to explain the chorus again and again in the explaination of the verses. For example, if you have already explained the chorus in the explaination of the ffirst verse, then you don't need to explain the chorus again in the explaination of the second verse, third verse, etc.
-        D. This task can be very large in size, so, you answer this task in as many lines you want.
+        B. You may try to also explain the meaning of the chorus, bridge, etc. of the song {song_name} by {artist} whose lyrics are given above. for example, you can say that the chorus of the song is about the following things: ((chorus explaination)).
+        C. You may try to explain the chorus only once of the song {song_name} by {artist} whose lyrics are given above.. You don't need to explain the chorus again and again in the explaination of the verses. For example, if you have already explained the chorus in the explaination of the ffirst verse, then you don't need to explain the chorus again in the explaination of the second verse, third verse, etc.
+        D. This task is very important for me. Please try to do this task for me. I will be very thankful to you if you do this task for me.
 
     3. Try to give the complete review of the song {song_name} by {artist} whose lyrics are given above."""
 
@@ -26,3 +26,43 @@ Now, please try to do the following task for me:
         f.write(prompt)
 
     return prompt
+
+def codex_prompt(lyrics, artist,song_name):
+    prompt = f"""I am try to make a blog which contains the analysis, review, explanation etc of the song {song_name} by {artist}.
+
+Lyrics for {song_name} by {artist} are as follows:
+
+
+{lyrics}
+
+Now, please try to do the following task for me:
+    # 4. Try to give the mood of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 5. Try to give the genre of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 6. Try to give the theme of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 7. Try to give the message of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 8. Try to give the story of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 9. Try to give the meaning of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+    # 10. Try to give the complete review of the song "Charlie Be Quiet!" by Charlie Puth whose lyrics are given above.
+
+    1. Try to give a brief summary of the song {song_name} by {artist}.
+    2. Try to give the verse-by-verse, complete, in-depth explaination and meaning for the song {song_name} by {artist} whose lyrics are given above. Other details for this are:
+
+        A. Try to explain the meaning of the first verse, second verse, third verse, etc. of the song {song_name} by {artist} whose lyrics are given above. for example, you can say that the first verse of the song is about the following things: ((first verse explaination)).
+        B. You may try to also explain the meaning of the chorus, bridge, etc. of the song {song_name} by {artist} whose lyrics are given above. for example, you can say that the chorus of the song is about the following things: ((chorus explaination)).
+        C. You may try to explain the chorus only once of the song {song_name} by {artist} whose lyrics are given above.. You don't need to explain the chorus again and again in the explaination of the verses. For example, if you have already explained the chorus in the explaination of the ffirst verse, then you don't need to explain the chorus again in the explaination of the second verse, third verse, etc.
+        D. This task is very important for me. Please try to do this task for me. I will be very thankful to you if you do this task for me.
+
+    3. Try to give the complete analysis of the song {song_name} by {artist} whose lyrics are given above.
+    4. Try to give the complete mood of the song {song_name} by {artist} whose lyrics are given above.
+    5. Try to give the complete genre of the song {song_name} by {artist} whose lyrics are given above.
+    6. Try to give the complete theme of the song {song_name} by {artist} whose lyrics are given above.
+    7. Try to give the complete message of the song {song_name} by {artist} whose lyrics are given above.
+    8. Try to give the complete story of the song {song_name} by {artist} whose lyrics are given above.
+    9. Try to give the complete meaning of the song {song_name} by {artist} whose lyrics are given above.
+    10. Try to give the complete review of the song {song_name} by {artist} whose lyrics are given above."""
+
+    with open("prompt.txt", "w", encoding="utf-8") as f:
+        f.write(prompt)
+
+    return prompt
+
