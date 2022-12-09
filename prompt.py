d87080f16fcd743cb9b043c296090a807c62d243
Chirag <76880977+chirag127@users.noreply.github.com>
2022-12-09 18:58:11 +0530

    only 3

--- prompt.py
+++ prompt.py
@@ -18,15 +18,9 @@ Now, please try to do the following task for me:
         A. Try to explain the meaning of the first verse, second verse, third verse, etc. of the song {song_name} by {artist} whose lyrics are given above. for example, you can say that the first verse of the song is about the following things: ((first verse explaination)).
         B. You may try to also explain the meaning of the chorus, bridge, etc. of the song. for example, you can say that the chorus of the song is about the following things: ((chorus explaination)).
         C. You may try to explain the chorus only once. You don't need to explain the chorus again and again in the explaination of the verses. For example, if you have already explained the chorus in the explaination of the ffirst verse, then you don't need to explain the chorus again in the explaination of the second verse, third verse, etc.
+        D. This task can be very large in size, so, you answer this task in as many lines you want.
 
-    3. Try to give the complete analysis of the song {song_name} by {artist} whose lyrics are given above.
-    4. Try to give the mood of the song {song_name} by {artist} whose lyrics are given above.
-    5. Try to give the genre of the song {song_name} by {artist} whose lyrics are given above.
-    6. Try to give the theme of the song {song_name} by {artist} whose lyrics are given above.
-    7. Try to give the message of the song {song_name} by {artist} whose lyrics are given above.
-    8. Try to give the story of the song {song_name} by {artist} whose lyrics are given above.
-    9. Try to give the meaning of the song {song_name} by {artist} whose lyrics are given above.
-    10. Try to give the complete review of the song {song_name} by {artist} whose lyrics are given above."""
+    3. Try to give the complete review of the song {song_name} by {artist} whose lyrics are given above."""
 
     with open("prompt.txt", "w", encoding="utf-8") as f:
         f.write(prompt)
