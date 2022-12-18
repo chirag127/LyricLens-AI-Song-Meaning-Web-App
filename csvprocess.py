af27a7687b3aba333c3426827d7cfb775b6ea6c4
chirag127 <whyiswhen@gmail.com>
2022-12-18 07:50:16 +0000

    don't do less than 15

--- csvprocess.py
+++ csvprocess.py
@@ -8,7 +8,7 @@ from prompt import return_prompt
 
 
 def get_lyrics_explaination_and_lyrics_csv(artist, title, album, date, year, lyric):
-    delay = 15
+    delay = 20
 
     try:
 
--- orchard.py
+++ orchard.py
@@ -15,7 +15,7 @@ def line_5(prompt):  # sourcery skip: inline-immediately-returned-variable
         'authority': 'api.orchard.ink',
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'en-US,en;q=0.9',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjcxOTQ5MDkyLCJzdWIiOiI0NzUzMDZjNS1jOWEyLTRjNmMtYmE5MS05NGFiODIyYjdkMjgiLCJlbWFpbCI6InN0b3JhZ2VjNTZAZ21haWwuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJnb29nbGUiLCJwcm92aWRlcnMiOlsiZ29vZ2xlIl19LCJ1c2VyX21ldGFkYXRhIjp7ImF2YXRhcl91cmwiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BRWRGVHA3QkJWVDRZTlRIYXh2WVhhaWdRdktUa2NCdHFoeWZReEdwRUpENz1zOTYtYyIsImVtYWlsIjoic3RvcmFnZWM1NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZnVsbF9uYW1lIjoiQ2xvdWQgU3RvcmFnZSIsImlzcyI6Imh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL3VzZXJpbmZvL3YyL21lIiwibmFtZSI6IkNsb3VkIFN0b3JhZ2UiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUVkRlRwN0JCVlQ0WU5USGF4dllYYWlnUXZLVGtjQnRxaHlmUXhHcEVKRDc9czk2LWMiLCJwcm92aWRlcl9pZCI6IjEwNjcyMTk2Njg1OTI2MzY3MzQ4OSIsInN1YiI6IjEwNjcyMTk2Njg1OTI2MzY3MzQ4OSJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im9hdXRoIiwidGltZXN0YW1wIjoxNjcxMzQ0MjkyfV0sInNlc3Npb25faWQiOiI0MjlhZTRhMi1kODllLTQwZWYtOTE4OC1lOGVlYWQ2NTU5OWQifQ.O9lAODgRpBgmXd1sYWCR1xQdnpQ9CZjmD2VO7uePVK0',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjcxOTUyMjA1LCJzdWIiOiIwZWRhYTg2MS1hNzNkLTRhZTMtOGRjYS04ZTk2MjA2NmE0MDgiLCJlbWFpbCI6Inl0dHViZTM1QGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZ29vZ2xlIiwicHJvdmlkZXJzIjpbImdvb2dsZSJdfSwidXNlcl9tZXRhZGF0YSI6eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUVkRlRwN0dKTHRBaGZRc2puTndUNjhaektGRkVFN2Vzel8za3FfendHbGM9czk2LWMiLCJlbWFpbCI6Inl0dHViZTM1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmdWxsX25hbWUiOiJBYmhpbmF2IEdveWFsIiwiaXNzIjoiaHR0cHM6Ly93d3cuZ29vZ2xlYXBpcy5jb20vdXNlcmluZm8vdjIvbWUiLCJuYW1lIjoiQWJoaW5hdiBHb3lhbCIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BRWRGVHA3R0pMdEFoZlFzam5Od1Q2OFp6S0ZGRUU3ZXN6XzNrcV96d0dsYz1zOTYtYyIsInByb3ZpZGVyX2lkIjoiMTAzNTAwMTQ2NjI3MDg2NzA0MDE4Iiwic3ViIjoiMTAzNTAwMTQ2NjI3MDg2NzA0MDE4In0sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoib2F1dGgiLCJ0aW1lc3RhbXAiOjE2NzEzNDc0MDV9XSwic2Vzc2lvbl9pZCI6IjQ1YzlhZjVkLWQwNjktNGRmZS1hMjM4LWM3ZTI3MmIxMmZiZiJ9.Jh9V_eSd7mwUoPulvLgHJ10uM4TrDWzJjopsCIJID8U',
         'content-type': 'application/json',
         'origin': 'https://www.orchard.ink',
         'referer': 'https://www.orchard.ink/',
@@ -31,45 +31,45 @@ def line_5(prompt):  # sourcery skip: inline-immediately-returned-variable
     json_data = {
         'thread': {
             'id': 'default',
-            'createdAt': '2022-12-15T07:18:26.968Z',
+            'createdAt': '2022-12-18T07:11:21.315Z',
             'messages': {
                 'root': {
                     'id': 'root',
                     'index': 0,
-                    'createdAt': '2022-12-15T07:18:26.968Z',
+                    'createdAt': '2022-12-18T07:11:21.315Z',
                     'type': 'response',
                     'messageText': "Hi, I'm Orchard! Ask me anything. If you want to edit your writing, select some text and you'll see a preview of it below. Try using a template from the dropdown!",
                     'innerText': "Hi, I'm Orchard! Ask me anything. If you want to edit your writing, select some text and you'll see a preview of it below. Try using a template from the dropdown!",
                     'childrenIds': [
-                        'a73979b6-9176-4586-9574-77269167e1a3',
+                        '47fa4041-cd68-46cc-b901-532ecbeb71e2',
                     ],
                     'rejected': False,
                 },
-                'a73979b6-9176-4586-9574-77269167e1a3': {
-                    'id': 'a73979b6-9176-4586-9574-77269167e1a3',
+                '47fa4041-cd68-46cc-b901-532ecbeb71e2': {
+                    'id': '47fa4041-cd68-46cc-b901-532ecbeb71e2',
                     'index': 1,
-                    'createdAt': '2022-12-18T06:19:38.179Z',
+                    'createdAt': '2022-12-18T07:48:29.298Z',
                     'type': 'instruction',
-                    'messageText': prompt,
+                    'messageText': 'hi',
                     'innerText': "Hi, I'm Orchard! Ask me anything. If you want to edit your writing, select some text and you'll see a preview of it below. Try using a template from the dropdown!",
                     'childrenIds': [
-                        '0a1fae92-8fa0-4f90-8209-bbbee6fb26f2',
+                        '2159a744-9696-4a66-b020-d2a082452e05',
                     ],
                     'rejected': False,
                     'templateId': '2c77110a-5644-4a1e-bfe4-f0ed2aebc94a',
                     'iconKey': 'Custom',
                     'parentId': 'root',
                 },
-                '0a1fae92-8fa0-4f90-8209-bbbee6fb26f2': {
-                    'id': '0a1fae92-8fa0-4f90-8209-bbbee6fb26f2',
+                '2159a744-9696-4a66-b020-d2a082452e05': {
+                    'id': '2159a744-9696-4a66-b020-d2a082452e05',
                     'index': 1,
-                    'createdAt': '2022-12-18T06:19:38.183Z',
+                    'createdAt': '2022-12-18T07:48:29.302Z',
                     'type': 'response',
                     'messageText': 'Loading Response #1...',
                     'innerText': '',
                     'childrenIds': [],
                     'rejected': False,
-                    'parentId': 'a73979b6-9176-4586-9574-77269167e1a3',
+                    'parentId': '47fa4041-cd68-46cc-b901-532ecbeb71e2',
                 },
             },
             'numResponses': 2,
@@ -77,12 +77,16 @@ def line_5(prompt):  # sourcery skip: inline-immediately-returned-variable
         },
         'template_id': '2c77110a-5644-4a1e-bfe4-f0ed2aebc94a',
         'template_name': 'Custom',
-        'document_id': 'f8589a2e-c594-498d-8e1a-9ee8fdca3320',
+        'document_id': 'f3560a80-d099-46c4-9c25-63f83b04527b',
         'is_retry': False,
     }
 
     response = requests.post('https://api.orchard.ink/generate_responses', headers=headers, json=json_data)
 
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"thread":{"id":"default","createdAt":"2022-12-18T07:11:21.315Z","messages":{"root":{"id":"root","index":0,"createdAt":"2022-12-18T07:11:21.315Z","type":"response","messageText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","innerText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","childrenIds":["47fa4041-cd68-46cc-b901-532ecbeb71e2"],"rejected":false},"47fa4041-cd68-46cc-b901-532ecbeb71e2":{"id":"47fa4041-cd68-46cc-b901-532ecbeb71e2","index":1,"createdAt":"2022-12-18T07:48:29.298Z","type":"instruction","messageText":"hi","innerText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","childrenIds":["2159a744-9696-4a66-b020-d2a082452e05"],"rejected":false,"templateId":"2c77110a-5644-4a1e-bfe4-f0ed2aebc94a","iconKey":"Custom","parentId":"root"},"2159a744-9696-4a66-b020-d2a082452e05":{"id":"2159a744-9696-4a66-b020-d2a082452e05","index":1,"createdAt":"2022-12-18T07:48:29.302Z","type":"response","messageText":"Loading Response #1...","innerText":"","childrenIds":[],"rejected":false,"parentId":"47fa4041-cd68-46cc-b901-532ecbeb71e2"}},"numResponses":2,"numInstructions":1},"template_id":"2c77110a-5644-4a1e-bfe4-f0ed2aebc94a","template_name":"Custom","document_id":"f3560a80-d099-46c4-9c25-63f83b04527b","is_retry":false}'
+#response = requests.post('https://api.orchard.ink/generate_responses', headers=headers, data=data)
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
 #data = '{"thread":{"id":"default","createdAt":"2022-12-15T07:18:26.968Z","messages":{"root":{"id":"root","index":0,"createdAt":"2022-12-15T07:18:26.968Z","type":"response","messageText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","innerText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","childrenIds":["a73979b6-9176-4586-9574-77269167e1a3"],"rejected":false},"a73979b6-9176-4586-9574-77269167e1a3":{"id":"a73979b6-9176-4586-9574-77269167e1a3","index":1,"createdAt":"2022-12-18T06:19:38.179Z","type":"instruction","messageText":"j","innerText":"Hi, I\'m Orchard! Ask me anything. If you want to edit your writing, select some text and you\'ll see a preview of it below. Try using a template from the dropdown!","childrenIds":["0a1fae92-8fa0-4f90-8209-bbbee6fb26f2"],"rejected":false,"templateId":"2c77110a-5644-4a1e-bfe4-f0ed2aebc94a","iconKey":"Custom","parentId":"root"},"0a1fae92-8fa0-4f90-8209-bbbee6fb26f2":{"id":"0a1fae92-8fa0-4f90-8209-bbbee6fb26f2","index":1,"createdAt":"2022-12-18T06:19:38.183Z","type":"response","messageText":"Loading Response #1...","innerText":"","childrenIds":[],"rejected":false,"parentId":"a73979b6-9176-4586-9574-77269167e1a3"}},"numResponses":2,"numInstructions":1},"template_id":"2c77110a-5644-4a1e-bfe4-f0ed2aebc94a","template_name":"Custom","document_id":"f8589a2e-c594-498d-8e1a-9ee8fdca3320","is_retry":false}'
